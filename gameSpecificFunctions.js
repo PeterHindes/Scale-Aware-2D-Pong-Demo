feac437f17881eb197d71b6b909db7627b5c9a46
Peter Hindes <ph9214@gmail.com>
2017-11-15 14:03:00 -0700

    finished migration :) (no testing lol)

--- gameSpecificFunctions.js
+++ gameSpecificFunctions.js
@@ -2,9 +2,56 @@
 //Notation for the atom extension custom-folds to allow collapsing code.
 
 
-//<editor-fold> Setup functions
+//<editor-fold> Mass Operation functions
+
   function resetGame() {
     ballX = 50;
     ballY = 50;
   }
-//</editor-fold> Setup functions
+
+
+  function updateGameVariables() {
+    if ((ballX+ballDiameterPercent) >= 100 || ballX <= 0){
+      ballSpeed *= -1
+      console.log("Big");
+    }
+    //console.log(canvas.width);
+    //console.log(ballX);
+    ballX += fFM(ballSpeed,framesPerSecond);
+  }
+
+
+  function drawGameCanvas() {
+
+    //Update the game variables BEFORE rendering
+    updateGameVariables();
+
+    //Background
+    Rect('black',0,0,canvas.width,canvas.height);
+
+    //Ball
+    canvasContext.fillStyle = 'pink';
+    canvasContext.fillRect(cFS(ballX,'x',false),	cFS(ballY,'y',false),	cFS(ballDiameterPercent,'x',true),	cFS(ballDiameterPercent,'y',true));
+
+
+    //Player 1
+    Rect(
+      'grey' ,
+      cFS(0+(paddleEdgeOffsetPercent),'x',false) ,
+      cFS(50-intigerCenterCalc(paddleHeightPercent),'y',false) ,
+      cFSa(paddleWidthPercent,paddleHeightPercent,true).x ,
+      cFSa(paddleWidthPercent,paddleHeightPercent,true).y
+    );
+
+    //Player 2
+    Rect(
+      'grey' ,
+      cFS(100+(-paddleEdgeOffsetPercent),'x',false)+(-cFS(paddleWidthPercent,'x',true)) ,
+      cFS(50-intigerCenterCalc(paddleHeightPercent),'y',false) ,
+      cFSa(paddleWidthPercent,paddleHeightPercent,true).x ,
+      cFSa(paddleWidthPercent,paddleHeightPercent,true).y
+    );
+
+  }
+
+//</editor-fold> Mass Operation functions
