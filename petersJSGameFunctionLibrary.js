72b9fcf6f81124fab7691d94598e795a11ddf02f
Peter Hindes <ph9214@gmail.com>
2017-11-15 18:58:59 -0700

    spell check

--- petersJSGameFunctionLibrary.js
+++ petersJSGameFunctionLibrary.js
@@ -30,13 +30,13 @@
 
 //<editor-fold> Transformitive functions
 
-  // Calculates the mid point between a number and 0 (otherwise known as top or left) and rounds down on non intigers
-  function intigerCenterCalc(toCenter) {
+  // Calculates the mid point between a number and 0 (otherwise known as top or left) and rounds down on non Integers
+  function IntegerCenterCalc(toCenter) {
     return (Math.floor(toCenter / 2));
   }
 
-  // Calculates the mid point between 2 numers and rounds down on non intigers
-  function intigerMidpointCalc(point1,point2) {
+  // Calculates the mid point between 2 numers and rounds down on non Integers
+  function IntegerMidpointCalc(point1,point2) {
     return (Math.floor((point1 + point2) / 2 ));
   }
 
@@ -45,7 +45,7 @@
     return (movePerSecond/framesPerSecond);
   }
 
-  // Calculates pixel location based on percent of canvas (standard (top/left)=0% to (botom/right)=100%)
+  // Calculates pixel location based on percent of canvas (standard (top/left)=0% to (bottom/right)=100%)
   function canvasFreeScaling(percent,axis,maintainAspect){
     var Refrence;
 
@@ -77,7 +77,7 @@
 //<editor-fold> Aliases for functions with long names
 
   // cFS stands for Canvas Free Scaling
-  // Calculates pixel location based on percent of canvas (standard (top/left)=0% to (botom/right)=100%)
+  // Calculates pixel location based on percent of canvas (standard (top/left)=0% to (bottom/right)=100%)
   function cFS(percent,axis,maintainAspect) {
     return (canvasFreeScaling(percent,axis,maintainAspect));
   }
@@ -94,16 +94,16 @@
     return (frameFreeMovement(movePerSecond,framesPerSecond));
   }
 
-  // iCC stans for Intiger Center Calc(ulator)
-  // Calculates the mid point between a number and 0 (otherwise known as top or left) and rounds down on non intigers
+  // iCC stands for Integer Center Calc(ulator)
+  // Calculates the mid point between a number and 0 (otherwise known as top or left) and rounds down on non Integers
   function iCC(toCenter) {
-    return (intigerCenterCalc(toCenter));
+    return (IntegerCenterCalc(toCenter));
   }
 
-  // iMC stands for Intiger Midpoint Calc(ulator)
-  // Calculates the mid point between 2 numers and rounds down on non intigers
+  // iMC stands for Integer Midpoint Calc(ulator)
+  // Calculates the mid point between 2 numers and rounds down on non Integers
   function iMC(point1,point2) {
-    return (intigerMidpointCalc(point1,point2));
+    return (IntegerMidpointCalc(point1,point2));
   }
 
 //</editor-fold> Aliases for functions with long names
@@ -111,15 +111,15 @@
 
 //<editor-fold> Depricated and or Broken functions
 
-/*  //Calculates the mid point between a number and 0 (rounds down on non intigers (otherwise known as top or left depending on if you use the result or x or y))
-  function intigerCenterCalc(toCenter) {
+/*  //Calculates the mid point between a number and 0 (rounds down on non Integers (otherwise known as top or left depending on if you use the result or x or y))
+  function IntegerCenterCalc(toCenter) {
     //Check if center is inbetween pixels
     if ( (toCenter % 2) == 0 ) {
       return (toCenter / 2);
     } else if ( (toCenter % 2) == 1 ) {	//Default to top/left pixel if centering lands on mid pixel
       return ((toCenter / 2) - 0.5);
     } else {
-      console.log("Error Calculating intigerCenter. Check Your Code!");
+      console.log("Error Calculating IntegerCenter. Check Your Code!");
       return -50;
     }
   }
